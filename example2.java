bbc4b992b9aa3f6fec679f72aeff046ba2342913
tosiab <mtabednarz@gmail.com>
2024-02-08 14:51:11 -0500

    exes2/8

---
+++ example2.java
@@ -0,0 +1,8 @@
+public class example2{
+    public static void main(String[]args){
+        System.out.println("**************************");
+        System.out.println("*     Sue's Swimwear     *");
+        System.out.println("*SwimWithSue@swimwear.com*");
+        System.out.println("**************************");
+    }  
+}
---
+++ example3.java
@@ -0,0 +1,6 @@
+public class example3 {
+    public static void main(String[]args){
+        System.out.println("Tosia Bednarz\n");
+        System.out.println("Song: Left, right, left go right!\nBumper sicker: Happiness if a butterfly!");
+    }
+}
